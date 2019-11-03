b44a12f76d9390562b69ada95cc09cf91a4a35b1
Nicholas Harlow <equipitosolo@gmail.com>
2019-11-02 23:34:55 -0700

    Added a basic definition and declaration of the ByteInventory

--- src/ByteInventory.cc
+++ src/ByteInventory.cc
@@ -4,5 +4,24 @@
 
 ByteInventory::ByteInventory() {
   byteCounts_ = new int[NUM_ITEMS];
-  size_ = 0;
+  size_ = NUM_ITEMS;
+}
+
+ByteInventory::ByteInventory(ByteInventory &other) {
+  byteCounts_ = new int[NUM_ITEMS];
+  size_ = other.size_;
+  for (int i = 0; i < size_; i++) {
+    byteCounts_[i] = other.byteCounts_[i];
+  }
+}
+
+ByteInventory::~ByteInventory() {
+  delete[] byteCounts_;
+}
+
+ByteInventory &ByteInventory::operator=(ByteInventory &other) {
+  size_ = other.size_;
+  for (int i = 0; i < size_; i++) {
+    byteCounts_[i] = other.byteCounts_[i];
+  }
 }
--- src/ByteInventory.h
+++ src/ByteInventory.h
@@ -2,17 +2,34 @@
 
 #define NUM_ITEMS 258
 
+// ByteInventory that contains a count of each byte.
 class ByteInventory {
  public:
+  // Constructers etc:
   // Creates a ByteInventory with full counts of 0. 
   ByteInventory();
   // Copies the current ByteInventory.
-  ByteInventory(ByteInventory &other);
+  ByteInventory(const ByteInventory &other);
   // Cleans up the ByteInventory.
   ~ByteInventory();
+
+  // Operators:
   // Overridden assignment operator.  
   ByteInventory &operator=(ByteInventory &other);
+
+  // Increment operator
+  ByteInventory &operator+=(ByteInventory &increment);
+
+  // Decrement operator
+  ByteInventory &operator-=(ByteInventory &increment);
+
  private:
   int *byteCounts_;
   int size_;
 };
+
+ByteInventory operator+(const ByteInventory &first, 
+                        const ByteInventory &second);
+
+ByteInventory operator-(const ByteInventory &first, 
+                        const ByteInventory &second);

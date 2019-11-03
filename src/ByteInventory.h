16d342ae2ed59c7f6b6b21fa5b1cc8dac28b4334
Nicholas Harlow <equipitosolo@gmail.com>
2019-11-03 11:29:03 -0800

    Finished base implementation of the ByteInventory. Might need some things added in the future.

--- src/ByteInventory.h
+++ src/ByteInventory.h
@@ -1,5 +1,7 @@
 // Copyright Nicholas Harlow
 
+#include <string>
+
 #define NUM_ITEMS 258
 
 // ByteInventory that contains a count of each byte.
@@ -13,15 +15,22 @@ class ByteInventory {
   // Cleans up the ByteInventory.
   ~ByteInventory();
 
+  // Add a new item to the inventory.
+  ByteInventory &addByte(int byte, int count);
+
   // Operators:
   // Overridden assignment operator.  
   ByteInventory &operator=(const ByteInventory &other);
 
   // Increment operator
-  ByteInventory operator+=(const ByteInventory &increment);
+  ByteInventory &operator+=(const ByteInventory &increment);
 
   // Decrement operator
-  ByteInventory operator-=(const ByteInventory &increment);
+  ByteInventory &operator-=(const ByteInventory &increment);
+
+  // toString, printing a list of the byte values and the associated counts
+  // if the count is > 0.
+  std::string toString() const;
 
  private:
   int *byteCounts_;
